6bbda5b1c13147717de6dfa4c0606d135dd76978
yutinglin <smile417781@gmail.com>
2023-02-08 19:35:36 +0800

    project setup

---
+++ src/test/java/com/example/coindesk/CoindeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coindesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
